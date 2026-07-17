6b31372237c20b957dc23f1ecd62adf1dde0e988
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-17 18:11:14 +0800

    Bump native app to Alpha 3

--- v2/app/build.gradle.kts
+++ v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.xgl34222220.baize"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20002
-        versionName = "2.0.0-alpha02"
+        versionCode = 20003
+        versionName = "2.0.0-alpha03"
     }
 
     buildFeatures {
