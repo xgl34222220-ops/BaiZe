9614e9f77b961998e64287df6511a727fe3f7236
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-19 18:47:29 +0800

    release(v2): set stable 2.0.0 app version

--- v2/app/build.gradle.kts
+++ v2/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.xgl34222220.baize"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22290
-        versionName = "2.0.0-alpha42.9"
+        versionCode = 22300
+        versionName = "2.0.0"
     }
 
     buildFeatures {
