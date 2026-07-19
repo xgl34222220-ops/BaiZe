a20407d547e46f48a8554e5b9d5992c9f3739853
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-19 12:11:06 +0800

    chore(v2): bump app to Alpha 42.3

--- v2/app/build.gradle.kts
+++ v2/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.xgl34222220.baize"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22220
-        versionName = "2.0.0-alpha42.2"
+        versionCode = 22230
+        versionName = "2.0.0-alpha42.3"
     }
 
     buildFeatures {
