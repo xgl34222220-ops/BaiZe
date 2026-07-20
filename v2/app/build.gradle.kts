89f1f846653c1fe2f1271ec60b483df9a7643871
xgl34222220-ops <xgl34222220@gmail.com>
2026-07-20 19:22:55 +0800

    chore(v2): bump organizer to Alpha 11

--- v2/app/build.gradle.kts
+++ v2/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.xgl34222220.baize"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22410
-        versionName = "2.1.0-alpha10"
+        versionCode = 22411
+        versionName = "2.1.0-alpha11"
     }
 
     buildFeatures {
