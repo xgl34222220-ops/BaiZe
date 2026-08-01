e781d7ecaa4a85c180a78f7fe0bcc185cf9bc13a
xgl34222220-ops <xgl34222220@gmail.com>
2026-08-01 08:30:01 +0800

    build: migrate macrobenchmark JVM target for Kotlin 2.4

--- v2/macrobenchmark/build.gradle.kts
+++ v2/macrobenchmark/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.test")
     id("org.jetbrains.kotlin.android")
@@ -17,9 +19,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
 
-    kotlinOptions {
-        jvmTarget = "17"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
